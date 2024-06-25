ca07d84c53e37c964c065f212d3896f2ef93b1e6
Marco Blanchette <marco.blanchette@liftoff-bio.com>
2024-06-25 00:50:25 +0000

    --

--- updateRout53.sh
+++ updateRout53.sh
@@ -2,34 +2,43 @@ TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-m
 api="http://169.254.169.254/latest/meta-data"
 header="X-aws-ec2-metadata-token: $TOKEN"
 
-ip=$(curl -s -H "${header}" ${api}/public-ipv4)
+instance_id=$(curl -s -H "${header}" ${api}/instance-id)
+az=$(curl -s -H "${header}" ${api}/placement/availability-zone)
 
+## Make sure to enable tags on instance
+resp=$(aws ec2 modify-instance-metadata-options \
+    --region ${az::-1} \
+    --instance-id ${instance_id} \
+    --instance-metadata-tags enabled)
+
+## Could make sure that resp has been executed...
+
+ip=$(curl -s -H "${header}" ${api}/public-ipv4)
+domain_name=$(curl -s -H "${header}" ${api}/tags/instance/domain_name)
+host_name=$(curl -s -H "${header}" ${api}/tags/instance/host_name)
 
 hostedZone=$( aws route53 list-hosted-zones-by-name \
   | jq '.HostedZones' \
-  | jq 'map(select(.Name == "liftoff-bio.com.").Id)[]'
+  | jq 'map(select(.Name == "'${domain_name}'.").Id)[]'
 )
-
-
 hostedZone=$(basename ${hostedZone//\"})
 
 
-echo $hostedZone
-# aws route53 change-resource-record-sets --hosted-zone-id ${hostedZone} --change-batch "$( cat << EOF 
-# {
-#     "Changes": [
-#         {
-#             "Action" : "UPSERT",
-#             "ResourceRecordSet": {
-#                 "Name": "gpu.liftoff-bio.com",
-#                 "Type": "A",
-#                 "TTL": 60,
-#                 "ResourceRecords": [
-#                     {"Value": "${ip}"}
-#                 ]
-#             }
-#         }
-#     ]
-# }
-# EOF
-# )"
+aws route53 change-resource-record-sets --hosted-zone-id ${hostedZone} --change-batch "$( cat << EOF 
+{
+    "Changes": [
+        {
+            "Action" : "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "${host_name}.${domain_name}",
+                "Type": "A",
+                "TTL": 60,
+                "ResourceRecords": [
+                    {"Value": "${ip}"}
+                ]
+            }
+        }
+    ]
+}
+EOF
+)"
